ec348834b442e42ec6fe71a67c2691e57db01f62
Charles Zaborski <zaborski.charles@gmail.com>
2023-07-02 15:17:52 +0200

    Merging two files with hardcoded urls.

--- mergePDF.js
+++ mergePDF.js
@@ -1,1 +1,15 @@
-console.log("init GIT");
+const PDFMerger = require('pdf-merger-js');
+const yargs = require("yargs");
+const args = yargs.argv
+
+const fileUrl1 = "./test/dok1.pdf";
+const fileUrl2 = "./test/dok2.pdf"
+const finalPDF = "./test/mergedPDF.pdf";
+
+const merger = new PDFMerger();
+
+(async () => {
+    await merger.add(fileUrl1);
+    await merger.add(fileUrl2);
+    await merger.save(finalPDF);
+})();
